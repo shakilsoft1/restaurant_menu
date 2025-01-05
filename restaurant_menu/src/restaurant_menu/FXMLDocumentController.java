a011cb49dd97f178453d017c18ac45119b91203b
Shakil Sarkar <149058203+shakilsoft1@users.noreply.github.com>
2025-01-06 02:10:57 +0600

    Login and Registration page Design
    login Page Design
    Registration page design

--- restaurant_menu/src/restaurant_menu/FXMLDocumentController.java
+++ restaurant_menu/src/restaurant_menu/FXMLDocumentController.java
@@ -4,31 +4,48 @@
  */
 package restaurant_menu;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author SHAKIL
- */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private TextField su_userName;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private PasswordField su_password;
+    @FXML
+    private Button su_signupBtn;
+    @FXML
+    private ComboBox<String> su_question;  // Specified the type here
+    @FXML
+    private PasswordField su_answer;
+    @FXML
+    private TextField si_username;
+    @FXML
+    private PasswordField si_password;
+    @FXML
+    private Button si_loginBtn;
+    @FXML
+    private Hyperlink si_forgot;
+
+   
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        
+    }
+
+    // Handle forgot password click
+    private void handleForgotPassword() {
+        System.out.println("Forgot Password clicked");
+    }
+
 }
